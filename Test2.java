cb02b9d9157d7fe01edd571900c9737f653c4d8d
jangHjoon <wkdguswns103@naver.com>
2021-05-22 20:49:38 +0900

    add

---
+++ Test2.java
@@ -0,0 +1,9 @@
+class Test2 
+{
+    public static void main(String[] args)
+    {
+        int i1 = 1;
+        int i2 = 3;
+        int i3 = 10;
+    }
+}
---
+++ Test2_2.java
@@ -0,0 +1,18 @@
+class Test2_2
+{
+    public static void main(String[] args)
+    {
+        int im2 = -2;
+        int im1 = -1;
+        int i1 = 1;
+        int i2 = 2;
+        int i3 = 3;
+        int i4 = 4;
+        int i5 = 5;
+        int i6 = 6;
+        int i7 = 7;
+        int i8 = 8;
+        int i9 = 9;
+        int i10 = 10;
+    }
+}
